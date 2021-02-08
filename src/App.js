b8d6007555d5fc64d1a281718b5655edfe955d52
vermad843 <vishnuverma7524@gmail.com>
2021-02-08 14:04:51 +0530

    todos not working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
 
 import './App.css';
 
 function App() {
 
    const [title,SetTitle] = useState('Hello World!!');
+   const [newTodo,setNewTodo] = useState('');
+   const [todos, setTodos] = useState([{
+     title : 'Learn React',
+     done : false
+   }]);
    
+
    const formSubmitted = (e) => {
-     e.preventDefault();     
-     console.log('hey');
+     e.preventDefault();  
+     setTodos({
+       todos : [
+         ...todos,
+         {
+           title : newTodo,
+           done : false
+         }
+       ]
+      });
    } 
+
+   const handleChanged = (e) => {
+      setNewTodo({
+        newTodo : e.target.value
+      })
+   }
 
   return (
     <div className="App">
         <h1>{title}</h1>
         <form onSubmit = {formSubmitted}>
              <label htmlFor = "newTodo">NewTodo</label>
-             <input id = "newTodo" name = "newTodo"/>
+             <input onChange = {handleChanged} id = "newTodo" name = "newTodo"/>
              <button type = "submit">
                 Add Todo
              </button>
         </form>
+        <ul>
+          {todos.map((todo) => {
+            return <li>
+               {todo.title}
+            </li>
+          })}
+        </ul>
     </div>
   );
 }
